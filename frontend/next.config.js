2570bccd8e2ebd09357c512724c76ea2d8e7912b
giansegg <gianpier.segovia@utec.edu.pe>
2026-05-18 01:38:06 -0500

    f5

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,30 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         //source: '/api/:path*',
+//         //destination: 'http://localhost:5000/api/:path*',
+//         source: '/api/:path*',
+//         destination: '/api/:path*',
+//       },
+//     ]
+//   },
+//   webpack(config) {
+//     // Enable async WASM for @hpcc-js/wasm (Graphviz)
+//     config.experiments = {
+//       ...config.experiments,
+//       asyncWebAssembly: true,
+//       layers: true,
+//     }
+//     return config
+//   },
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        //source: '/api/:path*',
-        //destination: 'http://localhost:5000/api/:path*',
-        source: '/api/:path*',
-        destination: '/api/:path*',
-      },
-    ]
-  },
   webpack(config) {
     // Enable async WASM for @hpcc-js/wasm (Graphviz)
     config.experiments = {
